a8ba71298e36cd1bb535420c75f4684a00044fee
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-30 16:43:01 +0100

    Fully test SVG injection

--- tests/svg-test.js
+++ tests/svg-test.js
@@ -1,1 +1,9 @@
+SVGInject.setOptions({
+    afterInject: (img, svg) => {
+        svg.addEventListener("click", (e) => {
+            e.target.classList.toggle("toggled")
+        })
+    }
+})
+
 SVGInject(document.querySelectorAll(".svg-inject"));
