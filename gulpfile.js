4a04ec6bf2a136dabd81fce1260352a4d28a97d1
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-30 23:34:56 +0100

    Setup initial project structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 
 // name this (build function) as you'd like
 function buildStyles() {
-    return src("src/sass/style.scss") // relative path to source
+    return src("sass/style.scss") // relative path to source
         .pipe(sass())
         .pipe(dest("dist")) // relative path to destination folder
 }
 
 // name this (watcher function) as you'd like
 function watchTask() {
-    watch(["src/**/*.scss"], buildStyles) 
+    watch(["sass/*.scss"], buildStyles) 
 }
 
 exports.default = series(buildStyles, watchTask)
