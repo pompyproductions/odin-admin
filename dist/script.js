e4688629427fa14ed8985b4cdf9bf2437529758d
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-01 14:00:48 +0100

    Create tabs for mobile

--- dist/script.js
+++ dist/script.js
@@ -7,3 +7,17 @@ SVGInject.setOptions({
 })
 
 SVGInject(document.querySelectorAll(".svg-inject"));
+
+for (let elem of document.querySelectorAll(".grid-tabs > p")) {
+    elem.addEventListener("click", switchTab)
+}
+
+
+// event functions
+
+function switchTab(e) {
+    for (let elem of document.querySelectorAll(".grid-tabs > p")) {
+        elem.classList.remove("active")
+    };
+    e.target.classList.add("active");
+}
