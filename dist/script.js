a3796ce49aff8e75a565d69dccba6d62a8c4a763
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-02 02:08:05 +0100

    Add tab functionality

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,5 @@
+// SVG Injection
+
 SVGInject.setOptions({
     afterInject: (img, svg) => {
         if (svg.id === "hamburger") {
@@ -12,16 +14,47 @@ SVGInject.setOptions({
 
 SVGInject(document.querySelectorAll(".svg-inject"));
 
+
+// ---
+// Mobile tabs
+
+function toggleTabElement(node) {
+    for (let elem of document.querySelectorAll(".grid-tabs > li")) {
+        elem.classList.remove("active")
+    };
+    node.classList.add("active");
+}
+
+function switchTabContainer(id) {
+    for (let elem of document.querySelectorAll(".tab")) {
+        elem.classList.add("hidden");
+    }
+    currentTab = tabs[id];
+    currentTab.classList.remove("hidden");
+}
+
+function onTabClick(e) {
+    if (tabs[e.target.getAttribute("data-tab")] !== currentTab) {
+        toggleTabElement(e.target);
+        switchTabContainer(e.target.getAttribute("data-tab"))
+    }
+}
+
+const tabs = [
+    document.querySelector(".card-container-projects"),
+    document.querySelector(".card-container-announcements"),
+    document.querySelector(".card-container-trending")
+];
+let currentTab = tabs[0];
+tabs[0].classList.toggle("hidden");
+
 for (let elem of document.querySelectorAll(".grid-tabs > li")) {
-    elem.addEventListener("click", switchTab)
+    elem.addEventListener("click", onTabClick)
 };
 
-document.getElementById("hamburger").addEventListener("click", onHamburgerClick);
-window.addEventListener("keydown", (e) => {
-    if (e.key === "`") onHamburgerClick(e) ;
-})
 
-// event functions
+// ---
+// Hamburger
 
 function onHamburgerClick(e) {
     document.querySelector(".overlay").classList.toggle("hidden");
@@ -29,9 +62,7 @@ function onHamburgerClick(e) {
     document.querySelector("#hamburger").classList.toggle("active");
 }
 
-function switchTab(e) {
-    for (let elem of document.querySelectorAll(".grid-tabs > li")) {
-        elem.classList.remove("active")
-    };
-    e.target.classList.add("active");
-}
+document.getElementById("hamburger").addEventListener("click", onHamburgerClick);
+window.addEventListener("keydown", (e) => {
+    if (e.key === "`") onHamburgerClick(e) ;
+})
